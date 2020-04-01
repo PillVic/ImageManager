d0c0bde00639f18881d8a59a862caa920d232024
pillvic <16205111@hdu.edu.cn>
2020-04-01 20:47:12 +0800

    Mainwindow.ui done

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import sys
 from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QLabel
 from PyQt5.uic import loadUi
 
 class ImageManager(QMainWindow):
     def __init__(self):
         super(ImageManager, self).__init__()
         loadUi('MainWindow.ui', self)
-        self.statusBar().showMessage("Ready to Read Image")
-        self.ActionExit = self.findChild()
+        fileName = QFileDialog.getOpenFileName(self, "Choose File", "")[0]
+        self.statusBar().showMessage("Open File : "+fileName)
 
 if __name__=="__main__":
     app = QApplication(sys.argv)
