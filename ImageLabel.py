da4db7c97542ff751c40d8901b2a2780d12be926
pillvic <16205111@hdu.edu.cn>
2020-04-21 14:42:03 +0800

    new class IamgeLabel create

---
+++ ImageLabel.py
@@ -0,0 +1,22 @@
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel
+from PyQt5.QtCore import pyqtSignal
+import cv2
+class ImageLabel(QLabel):
+    pts = []
+    getPoly = False
+    polyDone = pyqtSignal(str)
+    #鼠标点击事件
+    def mousePressEvent(self,event):
+        point = event.pos().x(), event.pos().y()
+        if self.getPoly == False:
+            return
+        self.pts.append(point)
+        print(point)
+        if len(self.pts) == 4 and self.getPoly:
+            self.getPoly = False
+            self.polyDone.emit("points get")
+    def getPts(self):
+        return self.pts
+    def setPoly(self):
+        self.pts.clear()
+        self.getPoly = True
